7efa0ce5af5b1f44050cb2ba3d61f87a592a1fd9
DonkeyFish456 <stancalderon@gmail.com>
2016-11-15 07:37:09 -0800

    add method to submit checkpoint assignments

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,10 +1,10 @@
 module Roadmap
   def get_checkpoint(checkpoint_id)
     headers = {
-        headers: {
-            'authorization' => @auth_token,
-            'content_type' => 'application/json'
-        }
+      headers: {
+        'authorization' => @auth_token,
+        'content_type' => 'application/json'
+      }
     }
     response = self.class.get("/checkpoints/#{checkpoint_id}", headers)
     parsed_response = JSON.parse(response.body)
@@ -12,12 +12,24 @@ module Roadmap
 
   def get_roadmap(roadmap_id)
     headers = {
-        headers: {
-            'authorization' => @auth_token,
-            'content_type' => 'application/json'
-        }
+      headers: {
+        'authorization' => @auth_token,
+        'content_type' => 'application/json'
+      }
     }
     response = self.class.get("/roadmaps/#{roadmap_id}", headers)
+    parsed_response = JSON.parse(response.body)
+  end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    body = {
+      checkpoint_id: checkpoint_id,
+      assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link,
+      comment: comment,
+      enrollment_id: get_me['current_enrollment']['id']
+    }
+    response = self.class.post('/checkpoint_submissions', headers: {"authorization" => @auth_token}, body: body)
     parsed_response = JSON.parse(response.body)
   end
 end
